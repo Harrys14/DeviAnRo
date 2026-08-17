3047aa687bb6b46e5418952d8c0c5f0fd747fa21
Harryson <roabecera1414@gmail.com>
2026-08-17 15:41:39 -0500

    Problema de scroll solucionado, cuando bajaba y entrabaj a otra pagina se quedaba en el mismo lugar en vez de subir hasta el principio, para eso se creo scrolltotopo.jsx

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 import Login from "../pages/Login";
 import Registro from "../pages/Registro";
 import Home from "../pages/Home";
@@ -7,22 +8,70 @@ import Cuenta from "../pages/Cuenta";
 import Detalle from "../pages/Detalle";
 import Restaurante from "../pages/Restaurante";
 import MenuRestaurante from "../pages/MenuRestaurante";
+
 import { ProtectedRoute } from "../auth/ProtectedRoute";
+import ScrollToTop from "../components/ScrollToTop";
 
 function AppRouter() {
   return (
     <BrowserRouter>
-      <Routes>
-        {/* PÚBLICAS: se ven sin login */}
-        <Route path="/" element={<Navigate to="/home" replace />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/registro" element={<Registro />} />
-        <Route path="/restaurante" element={<Restaurante />} />
-        <Route path="/restaurante/:id" element={<MenuRestaurante />} />
-        <Route path="/detalle/:id" element={<Detalle />} />
 
-        {/* PROTEGIDAS: solo logueado */}
+      {/* =====================================================
+          VOLVER ARRIBA AL CAMBIAR DE PÁGINA
+      ===================================================== */}
+
+      <ScrollToTop />
+
+      <Routes>
+
+        {/* =====================================================
+            PÚBLICAS
+        ===================================================== */}
+
+        <Route
+          path="/"
+          element={
+            <Navigate
+              to="/home"
+              replace
+            />
+          }
+        />
+
+        <Route
+          path="/home"
+          element={<Home />}
+        />
+
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+
+        <Route
+          path="/registro"
+          element={<Registro />}
+        />
+
+        <Route
+          path="/restaurante"
+          element={<Restaurante />}
+        />
+
+        <Route
+          path="/restaurante/:id"
+          element={<MenuRestaurante />}
+        />
+
+        <Route
+          path="/detalle/:id"
+          element={<Detalle />}
+        />
+
+        {/* =====================================================
+            PROTEGIDAS
+        ===================================================== */}
+
         <Route
           path="/carrito"
           element={
@@ -31,6 +80,7 @@ function AppRouter() {
             </ProtectedRoute>
           }
         />
+
         <Route
           path="/cuenta"
           element={
@@ -40,9 +90,22 @@ function AppRouter() {
           }
         />
 
-        {/* Catch-all */}
-        <Route path="*" element={<Navigate to="/home" replace />} />
+        {/* =====================================================
+            CATCH-ALL
+        ===================================================== */}
+
+        <Route
+          path="*"
+          element={
+            <Navigate
+              to="/home"
+              replace
+            />
+          }
+        />
+
       </Routes>
+
     </BrowserRouter>
   );
 }
